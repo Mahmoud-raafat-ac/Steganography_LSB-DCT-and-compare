9f599b9ed6f0eccadd8980bc84157e8aa864561c
Mahmoud-raafat-ac <75928839+Mahmoud-raafat-ac@users.noreply.github.com>
2021-05-17 06:53:19 +0200

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,59 +1,41 @@
-from skimage.measure import compare_ssim as structSim
-import matplotlib.pyplot as plot
 import numpy
 import cv2
+import xlwt
 
+#MSE--start
+def meanSquareError(img1, img2):
+    error = numpy.sum((img1.astype('float') - img2.astype('float')) ** 2)
+    error /= float(img1.shape[0] * img1.shape[1])
+    return error
+#MSE--end
 
-def meanSquareError(im1, im2):
-	error = numpy.sum((im1.astype('float') - im2.astype('float')) ** 2)
-	error /= float(im1.shape[0] * im1.shape[1]);
+#PSNR--start
+def PSNR(img1, img2):
+    mse = meanSquareError(img1,img2)
+    if mse == 0:
+        return 100
+    return 10 * numpy.log10( numpy.square(255) / mse )
+#PSNR--end
 
-	return error
+#comparison and write xls
+original = cv2.imread('test.png')
+lsbEncoded = cv2.imread('LSB_test.png')
+dctEncoded = cv2.imread('DCT_test.png')
 
-def compareImages(im1, im2, title):
-	mse = meanSquareError(im1,im2)
-	ss = structSim(im1,im2,multichannel=True)
+book = xlwt.Workbook()
+sheet1=book.add_sheet("Sheet 1")
+style_string = "font: bold on , color red; borders: bottom dashed"
+style = xlwt.easyxf(style_string)
+sheet1.write(0, 0, "Original vs", style=style)
+sheet1.write(0, 1, "MSE", style=style)
+sheet1.write(0, 2, "PSNR", style=style)
+sheet1.write(1, 0, "LSB")
+sheet1.write(1, 1, meanSquareError(original, lsbEncoded))
+sheet1.write(1, 2, PSNR(original, lsbEncoded))
+sheet1.write(2, 0, "DCT")
+sheet1.write(2, 1, meanSquareError(original, dctEncoded))
+sheet1.write(2, 2, PSNR(original, dctEncoded))
 
-	# make figure
-	fig = plot.figure(title)
-	plot.suptitle('MSE: %.2f, SSIM: %.2f' % (mse,ss))
-
-	ax = fig.add_subplot(1,2,1)
-	plot.imshow(im1, cmap = plot.cm.gray)
-	plot.axis('off')
-
-	ax = fig.add_subplot(1,2,2)
-	plot.imshow(im2, cmap = plot.cm.gray)
-	plot.axis('off')
-
-	plot.show()
-
-def main():
-	original = cv2.imread('img/lenna.png')
-	lsbEncoded = cv2.imread('img/LSBlenna.png')
-	dctEncoded = cv2.imread('img/DCTlenna.png')
-
-	original = cv2.cvtColor(original, cv2.COLOR_BGR2RGB)
-	lsbEncoded = cv2.cvtColor(lsbEncoded, cv2.COLOR_BGR2RGB)
-	dctEncoded = cv2.cvtColor(dctEncoded, cv2.COLOR_BGR2RGB)
-
-	# figure
-	fig = plot.figure("Images")
-	images = ('Original', original), ('LSB', lsbEncoded), ('DCT', dctEncoded)
-
-	for (i, (name, image)) in enumerate(images):
-		ax = fig.add_subplot(1,3,i+1)
-		ax.set_title(name)
-		plot.imshow(image, cmap=plot.cm.gray)
-		plot.axis('off')
-
-	plot.show()
-
-	# Compare all the images
-	compareImages(original, original, "Original vs Original")
-	compareImages(original, lsbEncoded, "Original vs LSB")
-	compareImages(original, dctEncoded, "Original vs DCT")
-
-
-if __name__=='__main__':
-	main()
+book.save("Comparison.xls")
+print("Comparison Results were saved as xls file!")
+#end
